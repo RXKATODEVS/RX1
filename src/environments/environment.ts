e2cbf25acc95206607948a0b311b72228a3d233d
Tom Cichy <ny@Toms-MacBook-Pro.local>
2018-05-19 10:44:47 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,6 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
@@ -13,3 +9,15 @@ export const environment = {
  * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD5t1mpon_ah59-Eu_hTvBNzu4sy7ehdlk",
+    authDomain: "rxkato1.firebaseapp.com",
+    databaseURL: "https://rxkato1.firebaseio.com",
+    projectId: "rxkato1",
+    storageBucket: "",
+    messagingSenderId: "142140011685"
+  }
+};
